3a7c864ec9f2e92b85289c1db640ff942d42497c
Peter Hedenskog <peter@soulgalore.com>
2012-10-15 23:14:03 +0200

    first commit

---
+++ test/alexa.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Run the top of the Alexa ranking
+../sitespeed.io http://www.google.com/
+../sitespeed.io http://www.facebook.com/
+../sitespeed.io http://www.youtube.com/
+../sitespeed.io http://us.yahoo.com/
+../sitespeed.io http://www.baidu.com
+../sitespeed.io http://en.wikipedia.org
+../sitespeed.io https://login.live.com/
+../sitespeed.io http://www.twitter.com
+../sitespeed.io http://www.qq.com
+../sitespeed.io http://www.amazon.com
+../sitespeed.io http://www.linkedin.com/
+../sitespeed.io http://www.blogger.com/home
