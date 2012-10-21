4b23ea63164f23805485cbc3785e3e7121bd7af7
Peter Hedenskog <peter@soulgalore.com>
2012-10-21 14:19:11 +0200

    different processes per site

--- test/alexa.sh
+++ test/alexa.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 # Run the top of the Alexa ranking
-../sitespeed.io http://www.google.com/
-../sitespeed.io http://www.facebook.com/
-../sitespeed.io http://www.youtube.com/
-../sitespeed.io http://us.yahoo.com/
-../sitespeed.io http://www.baidu.com
-../sitespeed.io http://en.wikipedia.org
-../sitespeed.io https://login.live.com/
-../sitespeed.io http://www.twitter.com
-../sitespeed.io http://www.qq.com
-../sitespeed.io http://www.amazon.com
-../sitespeed.io http://www.linkedin.com/
-../sitespeed.io http://www.blogger.com/home
+../sitespeed.io -u http://www.google.com/ -p 8
+../sitespeed.io -u http://www.facebook.com/ -p 8
+../sitespeed.io -u http://www.youtube.com/ -p 8
+../sitespeed.io -u http://us.yahoo.com/ -p 8
+../sitespeed.io -u http://www.baidu.com -p 6
+../sitespeed.io -u http://en.wikipedia.org -p 6
+../sitespeed.io -u https://login.live.com/ -p 2
+../sitespeed.io -u http://www.twitter.com -p 8
+../sitespeed.io -u http://www.qq.com -p §
+../sitespeed.io -u http://www.amazon.com -p 8
+../sitespeed.io -u http://www.linkedin.com/ -p 8
+../sitespeed.io -u http://www.blogger.com/home p -8
