b1d74f6c2167fbda61888025b7130ef1462faaad
Peter Hedenskog <peter@soulgalore.com>
2012-12-05 21:11:41 +0100

    removed live

--- test/alexa.sh
+++ test/alexa.sh
@@ -7,7 +7,6 @@
 ../sitespeed.io -u http://us.yahoo.com/ -p 8
 ../sitespeed.io -u http://www.baidu.com -p 6
 ../sitespeed.io -u http://en.wikipedia.org -p 6
-../sitespeed.io -u https://login.live.com/ -p 2
 ../sitespeed.io -u http://www.twitter.com -p 8
 ../sitespeed.io -u http://www.qq.com -p §
 ../sitespeed.io -u http://www.amazon.com -p 8
