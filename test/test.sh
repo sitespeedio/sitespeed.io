cff0b1af102f9a52d91ce349bf7fbf9ef8e8eb7f
Peter Hedenskog <peter@soulgalore.com>
2012-12-25 06:35:13 +0100

    added super simple test to see that the output files are created

---
+++ test/test.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# Set home
+cd "$(dirname ${BASH_SOURCE[0]})"
+
+ 
+UNIQUE_DIR=$(ls -1 ../build/sitespeed-result/ | head -n1);
+RESULT_DIR=$( cd ../build/sitespeed-result/$UNIQUE_DIR && pwd)
+
+
+files=( index.html errorurls.html pages.csv pages.html rules.html pages/1.html pages/2.html pages/3.html )
+
+for i in "${files[@]}"
+do
+   if [ ! -s $RESULT_DIR/$i ];
+      	then
+      		echo "The file $RESULT_DIR/$i  wasnt created"
+      	exit 1
+    fi		
+done
+
+echo 'The test finished succesfully'
+
+
