573f37206607d3f0fda9d69d366cefdfe133e24f
Peter Hedenskog <peter@soulgalore.com>
2013-04-15 06:18:53 +0200

    new handling for the junit xml

--- test/test.sh
+++ test/test.sh
@@ -9,7 +9,7 @@ DATE_DIR=$(ls -1 ../build/sitespeed-result/$DOMAIN_DIR | head -n1);
 RESULT_DIR=$( cd ../build/sitespeed-result/$DOMAIN_DIR/$DATE_DIR && pwd)
 
 
-files=( index.html errorurls.html pages.html rules.html summary-details.html pages/1.html pages/2.html pages/3.html daat/sitespeed-junit.xml)
+files=( index.html errorurls.html pages.html rules.html summary-details.html pages/1.html pages/2.html pages/3.html data/sitespeed-junit.xml)
 
 for i in "${files[@]}"
 do
