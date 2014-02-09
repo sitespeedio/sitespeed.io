2a6d945f39bc177ab99a1f50a8fc11af3c4ca520
JoongSeob Vito Kim <dorajistyle@dorajistyle.laptop>
2014-02-09 20:19:06 +0900

    Lazy rendering to get screenshot from the RESTful web application

--- dependencies/screenshot.js
+++ dependencies/screenshot.js
@@ -1,6 +1,9 @@
 var page = require('webpage').create(),
     address, output, size, w, h, agent, full;
-
+var resourceWait  = 1e3,
+    maxRenderWait = 1e4,
+    count         = 0,
+    renderTimeout;
 
 if (phantom.args.length < 5 || phantom.args.length > 6) {
     console.log('Usage: screenshot.js URL filename width height user-agent full');
@@ -13,19 +16,35 @@ if (phantom.args.length < 5 || phantom.args.length > 6) {
     agent = phantom.args[4];
     full = phantom.args[5];
 
+    function doClip() {
+        if (full != 'true')
+                page.clipRect = { left: 0, top: 0, width: w, height: h };
+			page.render(output);
+			phantom.exit();
+    }
+
+    page.onResourceRequested = function (req) {
+        count += 1;
+        clearTimeout(renderTimeout);
+    };
+ 
+    page.onResourceReceived = function (res) {
+        if (!res.stage || res.stage === 'end') {
+            count -= 1;
+	    if (count === 0) {
+	        renderTimeout = setTimeout(doClip, resourceWait);
+	    }
+	}
+    }; 
     page.viewportSize = { width: w , height: h};
     page.settings.userAgent = agent;  
     page.open(address, function (status) {
 	    if (status !== 'success') {
 		console.log('Unable to load the address!');
 	    } else {
-		window.setTimeout(function () {
-			
-            if (full != 'true')
-                page.clipRect = { left: 0, top: 0, width: w, height: h };
-			page.render(output);
-			phantom.exit();
-		    }, 200);
+		window.setTimeout(function (){
+            	doClip();
+        	}, maxRenderWait);
 	    }
 	});
 }
