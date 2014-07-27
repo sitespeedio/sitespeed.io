730aca85180ed6db17624ed3b4a5a41f681829a6
soulgalore <peter@soulgalore.com>
2014-07-27 21:55:03 +0200

    don't run if Java is missing

--- bin/sitespeed.js
+++ bin/sitespeed.js
@@ -4,12 +4,14 @@ var Runner = require("../lib/runner");
 var r = new Runner();
 
 require('whereis')('java', function searched(err) {
+  // yep, we still need Java for the crawler & browsertime
   if (err) {
-    return console.error(
-      'Could not find java, make sure it is installed in your $PATH');
+    console.error(
+      'Could not find Java, make sure it is installed in your $PATH');
   }
-});
-
-r.run(function() {
-  console.log("Finished callback");
+  else {
+    r.run(function() {
+      console.log("Finished callback");
+    });
+  }
 });
