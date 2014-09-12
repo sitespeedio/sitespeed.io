208f03f14d16bbed65f54f4e6beb58ebf07042ec
soulgalore <peter@soulgalore.com>
2014-09-12 21:08:04 +0200

    more config fixes, step 2 #432

--- bin/sitespeed.js
+++ bin/sitespeed.js
@@ -2,7 +2,7 @@
 var Sitespeed = require('../lib/sitespeed'),
 config = require('../lib/conf');
 
-var sitespeed = new Sitespeed(config);
+var sitespeed = new Sitespeed();
 
 require('whereis')('java', function searched(err) {
   // yep, we still need Java for the crawler & browsertime
@@ -11,9 +11,8 @@ require('whereis')('java', function searched(err) {
       'Could not find Java, make sure it is installed in your $PATH');
   }
   else {
-    sitespeed.run(function() {
-      // TODO this is only used to check that everything is ok
-      console.log('Finished callback');
+    sitespeed.run(config, function() {
+      console.log('Finished');
     });
   }
 });
