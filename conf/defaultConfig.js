893168c24cfeb865d41a2f024259e987889b5718
soulgalore <peter@soulgalore.com>
2014-11-30 22:38:18 +0100

    read JSON from files in the CLI closes #525

--- conf/defaultConfig.js
+++ conf/defaultConfig.js
@@ -7,7 +7,6 @@ var defaultConfig = {
   userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36',
   yslow: 'phantomjs/yslow-3.1.8-sitespeed.js',
   ruleSet: 'sitespeed.io-desktop',
-  limitFile: '../conf/desktopRules.json',
   profile: 'desktop',
   testData: 'all',
   no: 3,
