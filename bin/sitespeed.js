3be848a89fc316f0d7c1ef13a417f3a5c6d9dbef
Tobias Lidskog <tobias@lidskog.se>
2014-12-17 23:33:32 +0100

    Ensure errors are logged consistently before exit.
    Always exit with status 1 in case of error.

--- bin/sitespeed.js
+++ bin/sitespeed.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 
 var Sitespeed = require('../lib/sitespeed'),
-  config = require('../lib/cli');
+    config = require('../lib/cli'),
+    winston = require('winston');
 
 var sitespeed = new Sitespeed();
 
 require('whereis')('java', function searched(err) {
   // yep, we still need Java for the crawler & browsertime
   if (err) {
-    console.error(
-      'Could not find Java, make sure it is installed in your $PATH');
+    winston.loggers.get('sitespeed.io').error(
+        'Could not find Java, make sure it is installed in your $PATH');
+    process.exit(1);
   } else {
     sitespeed.run(config, function(err) {
       if (err) {
-        console.error(err);
+        winston.loggers.get('sitespeed.io').error(err);
         process.exit(1);
       }
     });
