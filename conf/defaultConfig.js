58dd94d9a9cf7fe9137bce759a2667dc83de6b50
soulgalore <peter@soulgalore.com>
2015-01-20 23:41:20 +0100

    renaming phantomjs to be headless #559

--- conf/defaultConfig.js
+++ conf/defaultConfig.js
@@ -5,7 +5,7 @@ var defaultConfig = {
   threads: 5,
   memory: 1024,
   userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36',
-  yslow: 'phantomjs/yslow-3.1.8-sitespeed.js',
+  yslow: 'headless/yslow-3.1.8-sitespeed.js',
   ruleSet: 'sitespeed.io-desktop',
   profile: 'desktop',
   testData: 'all',
