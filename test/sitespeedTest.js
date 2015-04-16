50e5a46cb5d9b5a43baa560ce6f4dc390c2a650b
soulgalore <peter@soulgalore.com>
2015-04-16 13:18:27 +0200

    full test, part 1

---
+++ test/sitespeedTest.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var assert = require('assert'),
+	Sitespeed = require('../lib/sitespeed');
+
+describe('sitespeed', function() {
+
+	describe('#run', function() {
+
+		var sitespeed = new Sitespeed();
+
+		it('should get error when passed incorrect configuration', function(done) {
+      sitespeed.run({}, function(err, result) {
+        err.should.exist;
+				done();
+			});
+		});
+
+
+		it('should get a result', function(done) {
+      sitespeed.run({
+				url: 'http://www.sitespeed.io',
+				deep: 0
+			}, function(err, result) {
+        result.should.exist;
+				done();
+			});
+		});
+
+
+	});
+
+});
