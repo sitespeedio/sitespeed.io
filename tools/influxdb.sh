5dc2dd11fa4b4014d74c8a870dbef18ca8d18178
Jonathan Lee <jonjohn1232009@gmail.com>
2016-05-02 23:45:18 -0400

    #908 - Replacing scripts for testing grafana, graphite, and influxdb with docker-compose.yml

--- tools/influxdb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-# Start local InfluxDB and Grafana instances, for testing InfluxDB reporting
-#  -e ADMIN_USER="root" -e INFLUXDB_INIT_PWD="root"
-docker run -d -p 8083:8083 -p 8086:8086 --expose 8090 --expose 8099 -e PRE_CREATE_DB="sitespeed" tutum/influxdb
-docker run -d -p 3000:3000 grafana/grafana
