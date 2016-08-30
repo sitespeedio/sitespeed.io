b2c60a893113da3d65c62441f0724c34b940c548
soulgalore <peter@soulgalore.com>
2016-08-30 11:41:54 +0200

    skip xvfb output

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -6,6 +6,6 @@ firefox --version
 
 echo 'Starting Xvfb ...'
 export DISPLAY=:99
-Xvfb :99  -ac -nolisten tcp -screen 0 1500x1200x16 &
+2>/dev/null 1>&2 Xvfb :99  -ac -nolisten tcp -screen 0 1500x1200x16 &
 sleep 1
 exec /usr/src/app/bin/sitespeed.js "$@"
