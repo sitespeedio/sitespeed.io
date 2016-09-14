7f88e68a32f77b0d846f1b025a20bef3e9541d2b
soulgalore <peter@soulgalore.com>
2016-09-14 21:54:54 +0200

    make it possible to set max old space size closes #1185

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -4,8 +4,10 @@ set -e
 google-chrome-stable --version
 firefox --version
 
+MAX_OLD_SPACE_SIZE="${MAX_OLD_SPACE_SIZE:-2048}"
+
 echo 'Starting Xvfb ...'
 export DISPLAY=:99
 2>/dev/null 1>&2 Xvfb :99  -ac -nolisten tcp -screen 0 1500x1200x16 &
 sleep 1
-exec /usr/src/app/bin/sitespeed.js "$@"
+exec node --max-old-space-size=$MAX_OLD_SPACE_SIZE /usr/src/app/bin/sitespeed.js "$@"
