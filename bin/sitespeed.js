b5bc7ad0c46a6af8fcb24ba7260dc9c2ea90e0d2
soulgalore <peter@soulgalore.com>
2016-10-07 21:29:25 +0200

    New signature for running a test.
    Lets try URLs and options. URLs isn't a perfect name since it can be
    file(s) also, so lets fix that. Next step would then be move out
    default cli options to each plugin as suggested by @tobli.

--- bin/sitespeed.js
+++ bin/sitespeed.js
@@ -6,8 +6,7 @@
 
 const cli = require('../lib/support/cli'),
   sitespeed = require('../lib/sitespeed'),
-  Promise = require('bluebird'),
-  loader = require('../lib/support/pluginLoader');
+  Promise = require('bluebird');
 
 require('longjohn');
 Promise.config({
@@ -20,18 +19,15 @@ process.exitCode = 1;
 let parsed = cli.parseCommandLine();
 let budgetFailing = false;
 
-loader.parsePluginNames(parsed.explicitOptions)
-  .then((pluginNames) => {
-    return sitespeed.run(pluginNames, parsed.options)
-      .then((result) => {
-        if (result.errors.length > 0) {
-          throw new Error('Errors while running:\n' + result.errors.join('\n'));
-        }
-        if (Object.keys(result.budgetResult.failing).length > 0) {
-          process.exitCode = 1;
-          budgetFailing = true;
-        }
-      });
+return sitespeed.run(parsed.options._, parsed.options)
+  .then((result) => {
+    if (result.errors.length > 0) {
+      throw new Error('Errors while running:\n' + result.errors.join('\n'));
+    }
+    if (Object.keys(result.budgetResult.failing).length > 0) {
+      process.exitCode = 1;
+      budgetFailing = true;
+    }
   })
   .then(() => {
     if (!budgetFailing) {
