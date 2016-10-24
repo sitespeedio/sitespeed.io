2134d118f6269724c14f29b3667f370bc9b7a8c1
soulgalore <peter@soulgalore.com>
2016-10-24 10:01:51 +0200

    first baby step

---
+++ release.sh
@@ -0,0 +1,15 @@
+ #!/bin/bash
+set -e
+# Super simple release script for sitespeed.io
+# Lets use it it for now and make it better over time :)
+# You need np for this to work
+# npm install --global np
+np $1
+
+PACKAGE_VERSION=$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+
+docker build --no-cache -t sitespeedio/sitespeed.io:$PACKAGE_VERSION -t sitespeedio/sitespeed.io:latest .
+
+docker login
+
+docker push sitespeedio/sitespeed.io:$PACKAGE_VERSION
