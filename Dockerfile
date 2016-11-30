1e744a5b764fbcdd092e152d10cb76d3862327da
Peter Hedenskog <peter@soulgalore.com>
2016-11-30 22:59:45 +0100

    Add video support (#1355)

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM sitespeedio/webbrowsers:firefox-50.0-chrome-54.0
+FROM sitespeedio/webbrowsers:firefox-50.0-chrome-54.0-1
 
 ENV SITESPEED_IO_BROWSERTIME__XVFB true
 ENV SITESPEED_IO_BROWSERTIME__CONNECTIVITY__ENGINE tc
--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -4,6 +4,16 @@ set -e
 google-chrome-stable --version
 firefox --version
 
+# Here's a hack for fixing the problem with Chrome not starting in time
+# See https://github.com/SeleniumHQ/docker-selenium/issues/87#issuecomment-250475864
+
+sudo rm -f /var/lib/dbus/machine-id
+sudo mkdir -p /var/run/dbus
+sudo service dbus restart > /dev/null
+service dbus status > /dev/null
+export $(dbus-launch)
+export NSS_USE_SHARED_DB=ENABLED
+
 MAX_OLD_SPACE_SIZE="${MAX_OLD_SPACE_SIZE:-2048}"
 
 exec node --max-old-space-size=$MAX_OLD_SPACE_SIZE /usr/src/app/bin/sitespeed.js "$@"
