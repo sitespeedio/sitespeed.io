6c787ef5b8e8d6d7509d93985202d787dbb60a4f
soulgalore <peter@soulgalore.com>
2017-01-04 10:56:29 +0100

    remove sitespeed result dir before building

--- release.sh
+++ release.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+
+# Remove the local sitespeed-result dir
+rm -fR sitespeed-result
+
 # Super simple release script for sitespeed.io
 # Lets use it it for now and make it better over time :)
 # You need np for this to work
