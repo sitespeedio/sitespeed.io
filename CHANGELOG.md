ca3d36e414f25dd9951fe889e1b4bcc969453ceb
Tobias Lidskog <tobias@lidskog.se>
2017-03-22 15:21:32 +0100

    Refactor result storage/url handling, and tweak S3
    * Allow S3 upload even when using custom outputFolder.
    * Add new s3.path option, to override the default storage path in the S3 bucket.
    * Change the default storage path in the S3 bucket to no longer include the prefix 'sitespeed-result’.
    * Remove broken/not useful progress load for S3 upload (was producing 100000+ log lines.
    * Extract result url building into separate module that’s made available to plugins via context.resultUrls.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,9 +11,14 @@
 * HTML tuning for smaller devices.
 * Added proxy parameters in the CLI from Browsertime.
 * Updated to [PerfCascade 1.2.2](https://github.com/micmro/PerfCascade/releases) (from 1.0.0)
+* Add new s3.path option, to override the default storage path in the S3 bucket.
+
+### Changed
+* The default upload path in S3 buckets no longer includes the prefix 'sitespeed-result'
 
 ### Fixed
 * Waterfall graphs loaded in Safari iOS didn't work.
+* Allow S3 upload even when using custom outputFolder.
 
 ## 4.7.0 2017-03-15
 ### Fixed
--- test/storageManagerTests.js
+++ test/storageManagerTests.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const StorageManager = require('../lib/support/storageManager'),
-  os = require('os'),
-  moment = require('moment'),
-  expect = require('chai').expect;
+const resultsStorage = require('../lib/support/resultsStorage');
+const moment = require('moment');
+const expect = require('chai').expect;
 
-function createManager(url) {
-  return new StorageManager(url, moment(), {resultBaseDir: os.tmpdir()});
+function createManager(url, outputFolder) {
+  return resultsStorage(url, moment(), outputFolder).storageManager;
 }
 
 describe('storageManager', function() {
@@ -24,24 +23,10 @@ describe('storageManager', function() {
     });
   });
 
-  describe('#pathFromRootToPageDir', function() {
-    it('should create path from site root', function() {
-      const storageManager = createManager('http://www.foo.bar');
-      const path = storageManager.pathFromRootToPageDir('http://www.foo.bar');
-      expect(path).to.equal('pages/www.foo.bar/');
-    });
-
+  describe('#getStoragePrefix', function() {
     it('should create path from url', function() {
-      const storageManager = createManager('http://www.foo.bar');
-      const path = storageManager.pathFromRootToPageDir('http://www.foo.bar/x/y/z.html');
-      expect(path).to.equal('pages/www.foo.bar/x/y/z.html/');
+      const storageManager = createManager('http://www.foo.bar', '/tmp/sitespeed.io/foo');
+      expect(storageManager.getStoragePrefix()).to.equal('foo');
     });
-
-    it('should create path from url with query string', function() {
-      const storageManager = createManager('http://www.foo.bar');
-      const path = storageManager.pathFromRootToPageDir('http://www.foo.bar/x/y/z?foo=bar');
-      expect(path).to.equal('pages/www.foo.bar/x/y/z/query-115ffe20/');
-    });
-
   });
 });
