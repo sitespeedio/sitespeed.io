29fbcaa2db6a55a7776bbf4a2ed638f46396d993
Tobias Lidskog <tobias@lidskog.se>
2017-03-28 15:12:30 +0200

    Refactor result storage/url handling, and tweak S3 (#1552)
    * Allow S3 upload even when using custom outputFolder.
    * Add new s3.path option, to override the default storage path in the S3 bucket.
    * Change the default storage path in the S3 bucket to no longer include the prefix 'sitespeed-result’.
    * Remove broken/not useful progress load for S3 upload (was producing 100000+ log lines.
    * Extract result url building into separate module that’s made available to plugins via context.resultUrls.

---
+++ test/pathToFolderTests.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const pathFromRootToPageDir = require('../lib/support/resultsStorage/pathToFolder');
+const expect = require('chai').expect;
+
+describe('pathFromRootToPageDir', function() {
+  it('should create path from site root', function() {
+    const path = pathFromRootToPageDir('http://www.foo.bar');
+    expect(path).to.equal('pages/www.foo.bar/');
+  });
+
+  it('should create path from url', function() {
+    const path = pathFromRootToPageDir('http://www.foo.bar/x/y/z.html');
+    expect(path).to.equal('pages/www.foo.bar/x/y/z.html/');
+  });
+
+  it('should create path from url with query string', function() {
+    const path = pathFromRootToPageDir('http://www.foo.bar/x/y/z?foo=bar');
+    expect(path).to.equal('pages/www.foo.bar/x/y/z/query-115ffe20/');
+  });
+});
