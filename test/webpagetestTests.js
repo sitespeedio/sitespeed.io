a12e4016af67fe8fb6311cf638df681c4f1c52ad
Peter Hedenskog <peter@soulgalore.com>
2017-10-29 10:41:06 +0100

    Feed plugins with filterRegistry (#1761)

--- test/webpagetestTests.js
+++ test/webpagetestTests.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const path = require('path');
 const expect = require('chai').expect;
 const messageMaker = require('../lib/support/messageMaker');
+const filterRegistry = require('../lib/support/filterRegistry');
 
 const wptResultPath = path.resolve(
   __dirname,
@@ -15,24 +16,20 @@ const wptResultPath = path.resolve(
 const wptResult = JSON.parse(fs.readFileSync(wptResultPath, 'utf8'));
 
 describe('webpagetest', () => {
+  const context = { messageMaker, filterRegistry };
+
   describe('plugin', () => {
     it('should require key for default server', () => {
-      expect(() => plugin.open({ messageMaker }, {})).to.throw();
+      expect(() => plugin.open(context, {})).to.throw();
     });
     it('should require key for public server', () => {
       expect(() =>
-        plugin.open(
-          { messageMaker },
-          { webpagetest: { host: 'www.webpagetest.org' } }
-        )
+        plugin.open(context, { webpagetest: { host: 'www.webpagetest.org' } })
       ).to.throw();
     });
     it('should not require key for private server', () => {
       expect(() =>
-        plugin.open(
-          { messageMaker },
-          { webpagetest: { host: 'http://myserver.foo' } }
-        )
+        plugin.open(context, { webpagetest: { host: 'http://myserver.foo' } })
       ).to.not.throw();
     });
   });
