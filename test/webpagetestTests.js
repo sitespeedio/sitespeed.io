e81be5d6899f82c09cae28ddeefcabebd5aed51a
Peter Hedenskog <peter@soulgalore.com>
2017-10-29 09:22:27 +0100

    Feed plugins with messageMaker (#1760)

--- test/webpagetestTests.js
+++ test/webpagetestTests.js
@@ -5,6 +5,7 @@ const aggregator = require('../lib/plugins/webpagetest/aggregator');
 const fs = require('fs');
 const path = require('path');
 const expect = require('chai').expect;
+const messageMaker = require('../lib/support/messageMaker');
 
 const wptResultPath = path.resolve(
   __dirname,
@@ -16,16 +17,22 @@ const wptResult = JSON.parse(fs.readFileSync(wptResultPath, 'utf8'));
 describe('webpagetest', () => {
   describe('plugin', () => {
     it('should require key for default server', () => {
-      expect(() => plugin.open({}, {})).to.throw();
+      expect(() => plugin.open({ messageMaker }, {})).to.throw();
     });
     it('should require key for public server', () => {
       expect(() =>
-        plugin.open({}, { webpagetest: { host: 'www.webpagetest.org' } })
+        plugin.open(
+          { messageMaker },
+          { webpagetest: { host: 'www.webpagetest.org' } }
+        )
       ).to.throw();
     });
     it('should not require key for private server', () => {
       expect(() =>
-        plugin.open({}, { webpagetest: { host: 'http://myserver.foo' } })
+        plugin.open(
+          { messageMaker },
+          { webpagetest: { host: 'http://myserver.foo' } }
+        )
       ).to.not.throw();
     });
   });
