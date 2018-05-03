16e0e951d0e41289b12334486012ad90347c52bd
Omri <omrilotan@users.noreply.github.com>
2018-05-03 10:32:29 +0300

    Update statsd template

--- test/graphiteTests.js
+++ test/graphiteTests.js
@@ -100,7 +100,7 @@ describe('graphite', function() {
       var data = generator.dataFromMessage(message, moment());
 
       expect(data).to.match(
-        /ns.summary.sub_domain_com.chrome.cable.domains.www.sitespeed.io.dns.median:[\d]{1,}\|t/
+        /ns.summary.sub_domain_com.chrome.cable.domains.www.sitespeed.io.dns.median:[\d]{1,}\|ms/
       );
     });
   });
