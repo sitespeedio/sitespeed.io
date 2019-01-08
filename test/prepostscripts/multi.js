e1156421a4ad643e60e89b2c5112353bc6024ac5
soulgalore <peter@soulgalore.com>
2019-01-08 20:40:37 +0100

    use latest bt

--- test/prepostscripts/multi.js
+++ test/prepostscripts/multi.js
@@ -1,7 +1,5 @@
 module.exports = async function(context, commands) {
-  await commands.measure.startAndNavigate('https://www.sitespeed.io');
-  await commands.measure.startAndNavigate('https://www.sitespeed.io/examples/');
-  return commands.measure.startAndNavigate(
-    'https://www.sitespeed.io/documentation/'
-  );
+  await commands.measure.start('https://www.sitespeed.io');
+  await commands.measure.start('https://www.sitespeed.io/examples/');
+  return commands.measure.start('https://www.sitespeed.io/documentation/');
 };
