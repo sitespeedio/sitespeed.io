b2287f76561addd349b61a7126c1328f7251b76f
soulgalore <peter@soulgalore.com>
2020-02-26 20:58:22 +0100

    lint

--- test/sustainablewebTests.js
+++ test/sustainablewebTests.js
@@ -14,14 +14,14 @@ const chai = require('chai');
 Promise.promisifyAll(fs);
 const expect = chai.expect;
 
-describe('sustainableWeb', function () {
-  describe('co2', function () {
+describe('sustainableWeb', function() {
+  describe('co2', function() {
     let har;
     const GREY_VALUE = 0.8193815884799998;
     // onst GREEN_VALUE = 0.54704300112;
     const MIXED_VALUE = 0.57128033088;
 
-    beforeEach(function () {
+    beforeEach(function() {
       return fs
         .readFileAsync(
           path.resolve(
@@ -37,29 +37,27 @@ describe('sustainableWeb', function () {
         });
     });
 
-    describe('perByte', function () {
+    describe('perByte', function() {
       const MILLION = 1000000;
-      it("returns a CO2 number for data transfer using 'grey' power", function () {
+      it("returns a CO2 number for data transfer using 'grey' power", function() {
         expect(co2.perByte(MILLION)).to.be.a('number');
         expect(co2.perByte(MILLION)).to.equal(1.1625599999999998);
       });
-      it("returns a lower CO2 number for data transfer from domains using entirely 'green' power", function () {
+      it("returns a lower CO2 number for data transfer from domains using entirely 'green' power", function() {
         expect(co2.perByte(MILLION, true)).to.be.a('number');
         expect(co2.perByte(MILLION, true)).to.be.below(1.1625599999999998);
         expect(co2.perByte(MILLION, true)).to.be.equal(0.77616);
       });
     });
 
-    describe('perPage', function () {
-      it('returns CO2 for total transfer for page', function () {
+    describe('perPage', function() {
+      it('returns CO2 for total transfer for page', function() {
         const pages = pagexray.convert(har);
         const pageXrayRun = pages[0];
 
-
-
         expect(co2.perPage(pageXrayRun)).to.be.equal(GREY_VALUE);
       });
-      it('returns lower CO2 for page served from green site', function () {
+      it('returns lower CO2 for page served from green site', function() {
         const pages = pagexray.convert(har);
         const pageXrayRun = pages[0];
         let green = [
@@ -75,7 +73,7 @@ describe('sustainableWeb', function () {
         ];
         expect(co2.perPage(pageXrayRun, green)).to.be.below(GREY_VALUE);
       });
-      it('returns a lower CO2 number where *some* domains use green power', function () {
+      it('returns a lower CO2 number where *some* domains use green power', function() {
         const pages = pagexray.convert(har);
         const pageXrayRun = pages[0];
         // green can be true, or a array containing entries
@@ -93,8 +91,8 @@ describe('sustainableWeb', function () {
         expect(co2.perPage(pageXrayRun, green)).to.be.equal(MIXED_VALUE);
       });
     });
-    describe('perDomain', function () {
-      it('shows object listing Co2 for each domain', function () {
+    describe('perDomain', function() {
+      it('shows object listing Co2 for each domain', function() {
         const pages = pagexray.convert(har);
         const pageXrayRun = pages[0];
         // console.log(Object.keys(pageXrayRun.domains))
@@ -116,11 +114,11 @@ describe('sustainableWeb', function () {
         expect(res).to.be.a('array');
         expect(res.length).to.equal(domains.length);
 
-        Object.values(res).forEach(function (val) {
+        Object.values(res).forEach(function(val) {
           expect(val.co2).to.be.a('number');
         });
       });
-      it('shows lower Co2 for green domains', function () {
+      it('shows lower Co2 for green domains', function() {
         const pages = pagexray.convert(har);
         const pageXrayRun = pages[0];
 
@@ -137,27 +135,27 @@ describe('sustainableWeb', function () {
         ];
         const res = co2.perDomain(pageXrayRun);
         const resWithGreen = co2.perDomain(pageXrayRun, greenDomains);
-        const sumRes = res.reduce(function (a, b) {
+        const sumRes = res.reduce(function(a, b) {
           return a + b.co2;
         }, 0);
-        const sumResWithGreen = resWithGreen.reduce(function (a, b) {
+        const sumResWithGreen = resWithGreen.reduce(function(a, b) {
           return a + b.co2;
         }, 0);
 
         expect(sumResWithGreen).to.be.below(sumRes);
       });
     });
-    describe('perContentType', function () {
-      it('shows a breakdown of emissions by content type', function () { });
+    describe('perContentType', function() {
+      it('shows a breakdown of emissions by content type', function() {});
     });
-    describe('dirtiestResources', function () {
+    describe('dirtiestResources', function() {
       it('shows the top 10 resources by CO2 emissions');
     });
   });
 
-  describe('hosting', function () {
+  describe('hosting', function() {
     let har;
-    beforeEach(function () {
+    beforeEach(function() {
       return fs
         .readFileAsync(
           path.resolve(
@@ -173,8 +171,8 @@ describe('sustainableWeb', function () {
         });
     });
 
-    describe('greenDomains', async function () {
-      it('it returns a list of green domains, when passed a page object', async function () {
+    describe('greenDomains', async function() {
+      it('it returns a list of green domains, when passed a page object', async function() {
         const pages = pagexray.convert(har);
         const pageXrayRun = pages[0];
 
@@ -197,7 +195,7 @@ describe('sustainableWeb', function () {
           'fonts.gstatic.com',
           'api.thegreenwebfoundation.org'
         ];
-        greenDomains.forEach(function (dom) {
+        greenDomains.forEach(function(dom) {
           expect(expectedGreendomains).to.include(dom);
         });
       });
