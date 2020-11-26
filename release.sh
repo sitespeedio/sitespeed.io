3762fc9a4e30867049defebc3b0dbe92f6e88137
soulgalore <peter@soulgalore.com>
2020-11-26 23:15:31 +0100

    Prepare for 16

--- release.sh
+++ release.sh
@@ -17,7 +17,7 @@ PACKAGE_VERSION=$(node -e 'console.log(require("./package").version)')
 
 docker build --no-cache -t sitespeedio/sitespeed.io:$PACKAGE_VERSION -t sitespeedio/sitespeed.io:latest .
 docker push sitespeedio/sitespeed.io:$PACKAGE_VERSION
-docker push sitespeedio/sitespeed.io:latest
+# docker push sitespeedio/sitespeed.io:latest
 
 docker build --no-cache -t sitespeedio/sitespeed.io:$PACKAGE_VERSION-slim --file Dockerfile-slim .
 docker push sitespeedio/sitespeed.io:$PACKAGE_VERSION-slim
@@ -26,11 +26,11 @@ docker build -t sitespeedio/sitespeed.io:$PACKAGE_VERSION-plus1 --build-arg vers
 docker push sitespeedio/sitespeed.io:$PACKAGE_VERSION-plus1
 
 # Update to latest version in the docs
-bin/sitespeed.js --version | tr -d '\n' > docs/_includes/version/sitespeed.io.txt
+#bin/sitespeed.js --version | tr -d '\n' > docs/_includes/version/sitespeed.io.txt
 
 # Generate the help for the docs
-bin/sitespeed.js --help > docs/documentation/sitespeed.io/configuration/config.md
+#bin/sitespeed.js --help > docs/documentation/sitespeed.io/configuration/config.md
 
 # Generate friendly names from code
-node release/friendlyNames.js > docs/documentation/sitespeed.io/configure-html/friendlynames.md
-node release/friendlyNamesBudget.js > docs/documentation/sitespeed.io/performance-budget/friendlynames.md
+#node release/friendlyNames.js > docs/documentation/sitespeed.io/configure-html/friendlynames.md
+#node release/friendlyNamesBudget.js > docs/documentation/sitespeed.io/performance-budget/friendlynames.md
