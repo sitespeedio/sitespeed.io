8f07fddc063d7944e2d3c4615d397fd9ecd82091
soulgalore <peter@soulgalore.com>
2022-02-07 22:34:00 +0100

    New action that builds the Docker containers

--- release.sh
+++ release.sh
@@ -13,16 +13,6 @@ docker login
 # npm install --global np
 np $* --no-yarn --branch main
 
-PACKAGE_VERSION=$(node -e 'console.log(require("./package").version)')
-
-docker buildx build --push --platform linux/arm64,linux/amd64 --no-cache -t sitespeedio/sitespeed.io:$PACKAGE_VERSION -t sitespeedio/sitespeed.io:latest .
-
-docker buildx build --push --platform linux/arm64,linux/amd64 --no-cache -t sitespeedio/sitespeed.io:$PACKAGE_VERSION-slim --file Dockerfile-slim .
-
-docker buildx build --push --platform linux/arm64,linux/amd64 -t sitespeedio/sitespeed.io:$PACKAGE_VERSION-plus1 --build-arg version=$PACKAGE_VERSION --file docker/Dockerfile-plus1 .
-
-docker buildx build --push --platform linux/arm64,linux/amd64 -t sitespeedio/sitespeed.io:$PACKAGE_VERSION-webpagetest --build-arg version=$PACKAGE_VERSION --file docker/Dockerfile-webpagetest .
-
 # Update to latest version in the docs
 bin/sitespeed.js --version | tr -d '\n' > docs/_includes/version/sitespeed.io.txt
 
