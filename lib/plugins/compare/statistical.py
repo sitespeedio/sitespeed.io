0935a64e04ebf186685418781e4f5a9eb2b3c8ed
Peter Hedenskog <peter@soulgalore.com>
2023-12-07 09:20:02 -0800

    Makes it easier to understand alternative settings

--- lib/plugins/compare/statistical.py
+++ lib/plugins/compare/statistical.py
@@ -6,15 +6,15 @@ def has_variability(sample):
     """Check if the sample has more than one unique value."""
     return len(set(sample)) > 1
 
-def perform_test(test_type, sample1, sample2, **kwargs):
+def perform_test(test_type, baseline, current, **kwargs):
     """Perform the statistical test based on the test type."""
-    if not has_variability(sample1) or not has_variability(sample2):
+    if not has_variability(baseline) or not has_variability(current):
         return None, "No variability"
 
     if test_type == 'wilcoxon':
-        return wilcoxon(sample1, sample2, **kwargs)
+        return wilcoxon(baseline, current, **kwargs)
     elif test_type == 'mannwhitneyu':
-        return mannwhitneyu(sample1, sample2, **kwargs)
+        return mannwhitneyu(baseline, current, **kwargs)
     else:
         raise ValueError("Invalid test type. Choose 'wilcoxon' or 'mannwhitneyu'.")
 
@@ -27,7 +27,7 @@ final_results = {}
 for group_name, metrics in input_data['metrics'].items():
     group_results = {}
     for metric_name, metric_data in metrics.items():
-        stat, p = perform_test(test_type, metric_data['sample1'], metric_data['sample2'], **options)
+        stat, p = perform_test(test_type, metric_data['baseline'], metric_data['current'], **options)
         if p == "No variability":
             group_results[metric_name] = {'statistic': "N/A", 'p-value': "N/A"}
         else:
